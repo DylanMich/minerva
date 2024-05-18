5de841af3a3c0b1ebc4e39109705a9141d7982ed
DylanMich <dylan.halley9@gmail.com>
2024-05-18 19:12:40 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,58 @@
+import logging
+from flask import Flask, request, render_template, jsonify
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.stem.porter import PorterStemmer
+import re
+from nltk.corpus import stopwords
+import nltk
+nltk.download('stopwords')
+import os
+
+
+app = Flask(__name__)
+ps = PorterStemmer()
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)  # Set logging level to INFO
+
+# Load NLTK stopwords
+stopwords = set(stopwords.words('english'))
+logging.info("NLTK stopwords loaded successfully.")
+
+# Load model and vectorizer
+try:
+    model = pickle.load(open('model2.pkl', 'rb'))
+    logging.info("Model loaded successfully.")
+except Exception as e:
+    logging.error("Error loading model: %s", e)
+
+try:
+    tfidfvect = pickle.load(open('tfidfvect2.pkl', 'rb'))
+    logging.info("Vectorizer loaded successfully.")
+except Exception as e:
+    logging.error("Error loading vectorizer: %s", e)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        try:
+            text = request.form['text']
+            prediction = predict(text)
+            return render_template('index.html', text=text, result=prediction)
+        except Exception as e:
+            return render_template('index.html', error=str(e))
+    return render_template('index.html')
+
+def predict(text):
+    review = re.sub('[^a-zA-Z]', ' ', text)
+    review = review.lower()
+    review = review.split()
+    review = [ps.stem(word) for word in review if not word in stopwords]
+    review = ' '.join(review)
+    review_vect = tfidfvect.transform([review]).toarray()
+    prediction = 'FAKE' if model.predict(review_vect) == 0 else 'REAL'
+    return prediction
+
+if __name__ == "__main__":
+    app.run(debug=True)
